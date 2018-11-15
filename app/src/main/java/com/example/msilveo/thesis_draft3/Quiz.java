281619035bc92d67d86ab642748e69514bb58cf2
jtsbellezas <33455812+jtsbellezas@users.noreply.github.com>
2018-11-15 08:26:59 +0800

    added comment test add

--- app/src/main/java/com/example/msilveo/thesis_draft3/Quiz.java
+++ app/src/main/java/com/example/msilveo/thesis_draft3/Quiz.java
@@ -10,4 +10,5 @@ public class Quiz extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
     }
+    //jusane
 }
---
+++ app/src/main/java/com/example/msilveo/thesis_draft3/TestAddFile.java
@@ -0,0 +1,4 @@
+package com.example.msilveo.thesis_draft3;
+
+public class TestAddFile {
+}
